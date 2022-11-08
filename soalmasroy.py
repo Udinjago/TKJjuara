8372fb2a2cd5c53adb4e1b013e3fb64172f26038
unknown <hanifxudin.864@gmail.com>
2022-11-08 08:37:26 +0700

    Hanif

---
+++ soalmasroy.py
@@ -0,0 +1,33 @@
+###soal2
+var1 = 10 
+var2 = 20
+var3 = var1 + var2
+print (var3)
+var4 = var2 - var1 
+print (var4)
+print (1+2)
+print (100-20)
+print (5*2)
+print (12/4)
+var5 = (var2 * var1)
+var6 = (var2 // var1)
+print (var4)
+print (var5)
+print (var6)
+
+###soal1
+Hanif = "Hello World To Python"
+print (Hanif)
+
+###soal3
+var7 = 100
+var8 = 50
+var9 = (var7 // var8)
+var10 = (var7 / var8)
+print (var9)
+print (var10)
+
+###soal4
+usiaku = "16 tahun" 
+kataku = "usiaku:"
+print (kataku, usiaku)
